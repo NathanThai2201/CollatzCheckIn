d60fc22c4309ad02e2c28b40da1d2d78ea4a6363
nehalsekhon <nsekhon1@ualberta.ca>
2024-02-26 21:25:01 -0700

    added navigation to new activity to edit user profile

--- app/src/main/java/com/example/collatzcheckin/attendee/profile/ProfileFragment.java
+++ app/src/main/java/com/example/collatzcheckin/attendee/profile/ProfileFragment.java
@@ -1,5 +1,6 @@
 package com.example.collatzcheckin.attendee.profile;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,6 +8,9 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.example.collatzcheckin.R;
 
@@ -17,14 +21,9 @@ import com.example.collatzcheckin.R;
  */
 public class ProfileFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    TextView emailText;
+    TextView nameText;
+    Button updateProfile;
 
     public ProfileFragment() {
         // Required empty public constructor
@@ -33,34 +32,36 @@ public class ProfileFragment extends Fragment {
     /**
      * Use this factory method to create a new instance of
      * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ProfileFragment.
      */
-    // TODO: Rename and change types and number of parameters
-    public static ProfileFragment newInstance(String param1, String param2) {
+    public static ProfileFragment newInstance() {
         ProfileFragment fragment = new ProfileFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
         return fragment;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_profile, container, false);
+
+        nameText = rootView.findViewById(R.id.username_text);
+        emailText = rootView.findViewById(R.id.email_text);
+
+        updateProfile = rootView.findViewById(R.id.updateButton);
+
+        updateProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getActivity(), UpdateProfileActivity.class);
+                startActivity(i);
+            }
+        });
+
+        return rootView;
     }
 }
