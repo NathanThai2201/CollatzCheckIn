b3740913d960e71031141b331d25480a56ccd381
Michael Carey <mcarey1@ualberta.ca>
2024-03-02 12:31:00 -0700

    Change file name

--- app/src/main/java/com/example/collatzcheckin/MainActivity.java
+++ app/src/main/java/com/example/collatzcheckin/MainActivity.java
@@ -10,7 +10,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.event_view_organizer);
 
 
         Button view_attendee = findViewById(R.id.view_attendee);
