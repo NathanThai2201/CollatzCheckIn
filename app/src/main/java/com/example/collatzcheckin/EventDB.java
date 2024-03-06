8a8670df2dfc6cabe5da05eb5d4d2d7d2cc4ad35
Michael Carey <mcarey1@ualberta.ca>
2024-03-06 15:52:33 -0700

    add event find

--- app/src/main/java/com/example/collatzcheckin/EventDB.java
+++ app/src/main/java/com/example/collatzcheckin/EventDB.java
@@ -1,8 +1,17 @@
 package com.example.collatzcheckin;
 
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
 
 import java.util.HashMap;
+import java.util.Map;
 
 public class EventDB {
     private EventDBConnector eventDB;
@@ -22,5 +31,32 @@ public class EventDB {
         eventData.put("Member Limit", Integer.toString(event.getMemberLimit()));
         eventRef.document(event.getEventTitle()).set(eventData);
 
+    }
+
+    public HashMap<String, String> getEvent(String eventName) {
+        HashMap<String, String> userData = new HashMap<>();
+        eventRef.document(eventName).get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                Log.d("TAG", task.toString());
+                if (task.isSuccessful()) {
+                    DocumentSnapshot document = task.getResult();
+                    if (document.exists()) {
+                        Log.d("TAG", "DocumentSnapshot data: " + document.getData());
+                        Log.d("TAG", "DocumentSnapshot data: " + document.getString("Name"));
+                        userData.put("Name", document.getString("Name"));
+                        userData.put("Email", document.getString("Email"));
+                        userData.put("Uid", document.getString("Uid"));
+
+                    } else {
+                        Log.d("TAG", "No such document");
+                    }
+                } else {
+                    Log.d("TAG", "get failed with ", task.getException());
+                }
+            }
+        });
+
+        return userData;
     }
 }
--- app/src/main/java/com/example/collatzcheckin/EventViewFragment.java
+++ app/src/main/java/com/example/collatzcheckin/EventViewFragment.java
@@ -12,14 +12,21 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
+import org.checkerframework.checker.units.qual.A;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
 
 public class EventViewFragment extends Fragment {
-    View view;
+    private EventDB db ;
+    private View view;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         view = inflater.inflate(R.layout.fragment_event_view, container, false);
         Button viewAttendeeButton = view.findViewById(R.id.view_attendee);
+        ArrayList<String> data = fetchEvent();
         viewAttendeeButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -28,5 +35,20 @@ public class EventViewFragment extends Fragment {
         });
         return view;
 
+    }
+
+    public ArrayList<String> fetchEvent() {
+        db = new EventDB();
+        HashMap<String, String> fetchData = db.getEvent("Main");
+        ArrayList<String> eventData = new ArrayList<>();
+        eventData.add(fetchData.get("eventTitle"));
+        eventData.add(fetchData.get("eventDescription"));
+        String date = fetchData.get("eventDate");
+        String[] parsedDate = date.split(" ");
+        eventData.add(parsedDate[1]);
+        eventData.add(parsedDate[2]);
+        eventData.add(parsedDate[parsedDate.length - 1]);
+
+        return eventData;
     }
 }
